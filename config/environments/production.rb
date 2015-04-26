6a07b86bbdb2282714b4f1dee6926a618632c6fb
Oscar <caicedo.os@gmail.com>
2015-04-26 14:45:57 -0500

    se corrige config/enviroments/production para estilos

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+config.serve_static_assets = true
+config.assets.compile = true
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
@@ -29,8 +32,7 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
-
+  
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
